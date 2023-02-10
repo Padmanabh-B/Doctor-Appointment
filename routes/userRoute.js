db693434184184b3a121b83c0ef808ce43a39116
Padmanabh Belavadi <pro.paddusonu@gmail.com>
2023-02-10 18:21:17 +0530

    added Login and Register With Protecting Home Route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,20 +1,68 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register',async (req,res)=>{
-    try {
-        
-    } catch (error) {
-        
+
+router.post("/register", async (req, res) => {
+  try {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res
+        .status(200)
+        .send({ message: "User Alredy Exits", success: false });
     }
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+    const newuser = new User(req.body);
+    await newuser.save();
+    res
+      .status(200)
+      .send({ message: "User Created Successfully", success: true });
+  } catch (error) {
+    res.status(400).send({ message: "Error in Creating User", success: false });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "User Does not Exists", success: false });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res
+        .status(200)
+        .send({ message: "Password is Incorrect", success: false });
+    } else {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+      });
+      res
+        .status(200)
+        .send({ message: "Login Successfully", success: true, data: token });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+    .status(200)
+    .send({ message: "Erron in Logging In", success: true, data: token });
+  }
+});
+
+router.post('get-user-inof-by-id', async()=>{
+  try {
+    
+  } catch (error) {
+    
+  }
 })
 
-router.post('/login',async (req,res)=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 require("./config/dbConfig").dbConnection()
+app.use(express.json())
 const userRoute = require('./routes/userRoute')
+
+app.use('/api/user',userRoute)
+
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use('api/users',userRoute)
+
+app.get("/",(req,res)=>{
+    res.send("This is Server")
+    console.log("Hello This is Server");
+})
 
 app.listen(PORT, ()=>{
     console.log(`Node Server Started at Port ${PORT}`);
