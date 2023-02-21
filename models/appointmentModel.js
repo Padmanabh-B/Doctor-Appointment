9d5a5b49c9f709730ac1b5359596ce13f4a5ab8f
Padmanabh Belavadi <pro.paddusonu@gmail.com>
2023-02-21 14:26:12 +0530

    added appointment model

---
+++ models/appointmentModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const appoinmentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    doctorId: {
+      type: String,
+      required: true,
+    },
+    doctorInfo: {
+      type: String,
+      required: true,
+    },
+    userInfo: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "pening",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const appoinmentModel = mongoose.model("appoinment", appoinmentSchema);
+
+module.exports = appoinmentModel;
